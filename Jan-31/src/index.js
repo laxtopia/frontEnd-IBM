5c0735133eede73b3a066f3f5990ffaab5a460c2
laxtopia <b.sailaxman41@gmail.com>
2022-01-31 17:33:56 +0530

    initial

---
+++ Jan-31/src/index.js
@@ -0,0 +1,32 @@
+
+var newElem = document.createElement("hi");
+newElem.textContent = "i am new element  "
+
+var parentElem = document.getElementById("app")
+parentElem.appendChild(newElem)
+
+console.log(newElem)
+
+
+// ###################################################
+
+// var childElem = document.getElementById("app");
+// var center = childElem.children;
+// console.log(center);
+
+
+// #################################################################################
+// document.getElementById("app").innerHTML = "Hello JavaScript!";
+
+// var gg  = document.getElementById('app');
+// var secondTarget = document.getElementsByTagName('p');
+// var thirdTarget = document.getElementsByClassName('para');
+// var fourthtarget = document.querySelector('.para');
+// var fifthTarget = document.querySelectorAll('.para')
+
+// console.log(gg);
+// console.log(secondTarget.firstChild);
+// console.log(thirdTarget);
+// console.log(fourthtarget);
+// console.log(fifthTarget)
+
